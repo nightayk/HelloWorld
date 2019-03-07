b882025434959ddc92752a1cad80eb54dfb281dc
Nightayk <43944986+nightayk@users.noreply.github.com>
2019-03-07 23:07:38 +0800

    Update Test01.java

--- Test01.java
+++ Test01.java
@@ -3,6 +3,6 @@ public class Test01 {
     //主函数
     public static void main(String[] args) {
         //输出Hello World
-        System.out.println("Hello World");
+        System.out.println("Hello World");//输出"你好,(罪恶的)世界"
     }
 }
